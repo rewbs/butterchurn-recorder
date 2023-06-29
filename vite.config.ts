02a1b8481c8120dbc09065c24050a90166f5bea9
Robin Fernandes <rfernandes@atlassian.com>
2023-06-29 15:34:42 +1000

    Revert "Fix production build."
    This reverts commit a14946b313499bf2d0eb97d557c1bdfb990b6fcb.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import solid from "solid-start/vite";
 import vercel from 'solid-start-vercel'
 import { defineConfig } from "vite";
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
   plugins: [
     solid({
       adapter: vercel({edge:false}),
     }),
-    nodePolyfills()
-  ],
-  ssr: {
-    external: ['fs']
-  },  
+  ],  
 })
